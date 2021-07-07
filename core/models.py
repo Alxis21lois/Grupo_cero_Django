33f615d5a0b06d7bb7a5ba0676b504d18ee1220a
Ale <sistemarestobar@hotmail.com>
2021-07-06 23:20:03 -0400

    agragandio mas vistas y img

--- core/models.py
+++ core/models.py
@@ -5,6 +5,9 @@ class Autor(models.Model):
     idAutor = models.IntegerField(primary_key=True,verbose_name="Id autor")
     nombre = models.CharField(max_length=250,verbose_name="Nombre autor")
     historia = models.TextField(verbose_name="Historia")
+
     def __str__(self):
         return  self.nombre
         return  self.historia
+
+        
