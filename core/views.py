e5a7a798d5bf58f61255339219122903c69ec960
Ale <sistemarestobar@hotmail.com>
2021-07-07 17:03:52 -0400

    arregalndo las rutas de url y modelo

--- core/views.py
+++ core/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
+from .models import Autor
 
 # Create your views here.
 def index(request):
     return render(request,'core/index.html')
 
 def prueba(request):
-        return render(request, "core/prueba.html")
-
-def andres_lupin(request):
-    return render(request,"core/tejedores/andres_lupin.html")
+        todos_autores = Autor.objects.all
+        return render(request, "core/prueba.html", {'todosA', todos_autores})
 
