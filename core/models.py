6bcc76389417342c4eeb799ffb0bde7e47c6ecc1
Alexis <al.vallejosv@duocuc.cl>
2021-07-06 20:39:28 -0400

    Se crea db

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    idAutor = models.IntegerField(primary_key=True,verbose_name="Id autor")
+    nombre = models.CharField(max_length=250,verbose_name="Nombre autor")
+    historia = models.TextField(verbose_name="Historia")
+    def __str__(self):
+        return  self.nombre
+        return  self.historia
--- pagina_artes/settings.py
+++ pagina_artes/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'pagina_artes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'grupo_cero_db',
+        'USER': 'Admin_GC',
+        'PASSWORD': 'alexis21',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
 
