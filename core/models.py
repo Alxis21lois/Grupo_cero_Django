e742d757ee2f8ed7c8b7a3bf73a362b227c34742
Ale <sistemarestobar@hotmail.com>
2021-07-10 00:51:19 -0400

    obra a admin

--- core/models.py
+++ core/models.py
@@ -6,12 +6,25 @@ class Autor(models.Model):
     nombre = models.CharField(max_length=250,verbose_name="Nombre autor")
     historia = models.TextField(verbose_name="Historia")
     img = models.ImageField(upload_to='autores', null=True)
-
-
     def __str__(self):
         return  self.nombre
         return  self.historia
         return  self.idAutor
         return  self.img
 
-        
+
+class Obra(models.Model):
+    idObra = models.IntegerField(primary_key=True,verbose_name="Id obra")
+    idAutor  = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=250,verbose_name="Nombre obra")
+    historia = models.TextField(verbose_name="Historia")
+    img = models.ImageField(upload_to='obras', null=True)
+
+
+
+    def __str__(self):
+        return  self.idObra
+        return  self.idAutor
+        return  self.nombre
+        return  self.historia
+        return  self.img
--- core/views.py
+++ core/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from .models import Autor
+from .models import Autor, Obra
 
 # Create your views here.
 
 
 def prueba(request):
         autores = Autor.objects.all()
-        datos = {"autores" : autores}
+        obras =   Obra.objects.all()
+        datos = {"autores" : autores, "obras" : obras}
         
         return render(request, "core/prueba.html", datos)
 
