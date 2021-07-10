84fb1b7404b9c6620371b504e128fe9c60f23368
Ale <sistemarestobar@hotmail.com>
2021-07-10 02:00:12 -0400

    borando grasa

--- core/models.py
+++ core/models.py
@@ -2,20 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Autor(models.Model):
-    idAutor = models.IntegerField(primary_key=True,verbose_name="Id autor")
+    idautor = models.IntegerField(primary_key=True,verbose_name="idautor")
     nombre = models.CharField(max_length=250,verbose_name="Nombre autor")
     historia = models.TextField(verbose_name="Historia")
     img = models.ImageField(upload_to='autores', null=True)
     def __str__(self):
         return  self.nombre
         return  self.historia
-        return  self.idAutor
         return  self.img
 
 
 class Obra(models.Model):
-    idObra = models.IntegerField(primary_key=True,verbose_name="Id obra")
-    idAutor  = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    idobra = models.IntegerField(primary_key=True,verbose_name="idobra")
+    idautor  = models.ForeignKey(Autor, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=250,verbose_name="Nombre obra")
     historia = models.TextField(verbose_name="Historia")
     img = models.ImageField(upload_to='obras', null=True)
@@ -23,8 +22,6 @@ class Obra(models.Model):
 
 
     def __str__(self):
-        return  self.idObra
-        return  self.idAutor
         return  self.nombre
         return  self.historia
-        return  self.img
+
